b75452c4ad8f31cb66ee1a75f18b139b006cba4b
Tales Montenegro <montenegro.tales@gmail.com>
2020-10-02 23:05:39 -0300

    feature(contants): Create basic and imported sale tax constants

---
+++ src/main/java/com/montenegro/taxcalculator/constants/Constants.java
@@ -0,0 +1,6 @@
+package com.montenegro.taxcalculator.constants;
+
+public class Constants {
+	public static final double BASIC_SALES_TAX = 0.1;
+	public static final double IMPORTED_SALES_TAX = 0.05;
+}
