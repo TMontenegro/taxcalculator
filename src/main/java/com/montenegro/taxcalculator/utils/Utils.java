4234cfd8e2c1b0a55843b9e092f10fbbcd7ab742
Tales Montenegro <montenegro.tales@gmail.com>
2020-10-05 00:51:28 -0300

    feature(round): Create a function to round values to nearest 5 percent

---
+++ src/main/java/com/montenegro/taxcalculator/utils/Utils.java
@@ -0,0 +1,9 @@
+package com.montenegro.taxcalculator.utils;
+
+public class Utils {
+	public static double round(double value) {
+		double nearestTo5PercentMultiplier = 20.0;
+		
+		return Math.round(value * nearestTo5PercentMultiplier) / nearestTo5PercentMultiplier;
+	}
+}
