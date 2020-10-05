b64edcb1d5825d20207c64420ac40d0f1614db29
Tales Montenegro <montenegro.tales@gmail.com>
2020-10-05 01:06:38 -0300

    feature(infrastrucure): Add CORS mappings and UTF-8 propertie

--- src/main/java/com/montenegro/taxcalculator/TaxCalculatorApplication.java
+++ src/main/java/com/montenegro/taxcalculator/TaxCalculatorApplication.java
@@ -2,10 +2,23 @@ package com.montenegro.taxcalculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TaxCalculatorApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TaxCalculatorApplication.class, args);
+	}
+	
+	@Configuration
+	public class WebConfig implements WebMvcConfigurer {
+
+		@Override 
+		public void addCorsMappings(CorsRegistry registry) {
+			registry.addMapping("/**")
+			.allowedMethods("GET", "POST");
+		}
 	}
 }
