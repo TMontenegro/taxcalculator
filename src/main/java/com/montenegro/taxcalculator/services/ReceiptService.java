bb75c1cde40e54abc646b303bed86dc4a6c5ef01
Tales Montenegro <montenegro.tales@gmail.com>
2020-10-05 01:04:03 -0300

    feature(receipt): Create build function to initialize products based on its types and generate receipt

--- src/main/java/com/montenegro/taxcalculator/services/ReceiptService.java
+++ src/main/java/com/montenegro/taxcalculator/services/ReceiptService.java
@@ -1,31 +1,64 @@
 package com.montenegro.taxcalculator.services;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
+import com.montenegro.taxcalculator.entities.AbstractProduct;
 import com.montenegro.taxcalculator.entities.Product;
 import com.montenegro.taxcalculator.entities.Receipt;
+import com.montenegro.taxcalculator.utils.Utils;
 
 @Service
 public class ReceiptService {
-	
+	private List<AbstractProduct> abstractProducts = new ArrayList<AbstractProduct>();
+
 	public Receipt create(List<Product> products) {
-		return new Receipt(this.sumTotal(products), this.sumTaxes(products), this.applyTaxes(products));
-	}
-	
-	public List<Product> applyTaxes(List<Product> products) {
-		return products;
+		this.build(products);
+
+		double totalTaxes = this.sumTaxes();
+		
+		products = applyTaxesOnProducts();
+		
+		double totalPrice = this.getTotalPrice();
+
+		return new Receipt(totalPrice, totalTaxes, products);
 	}
 
+	public void build(List<Product> products) {
+		for (Product product : products) {
+			if (AbstractProduct.isExempt(product.getCategory())) 
+				this.abstractProducts.add(new ExemptProductService(product));
+			
+			else 
+				this.abstractProducts.add(new NonExemptProductService(product));
+		}
+	}
 	
-	public double sumTotal(List<Product> products) {
-		return 0;
+	public double sumTaxes() {
+		double total = this.abstractProducts
+							.stream()
+							.mapToDouble(product -> product.sumTaxes())
+							.sum();
+
+		return Utils.round(total);
 	}
 
-	
-	public double sumTaxes(List<Product> products) {
-		return 0;
+	public List<Product> applyTaxesOnProducts() {
+		return this.abstractProducts
+					.stream()
+					.map(product -> product.applyTaxes())
+					.collect(Collectors.toList());
 	}
-	
+
+	public double getTotalPrice() {
+		double total = this.abstractProducts
+							.stream()
+							.mapToDouble(product -> product.sumTotal())
+							.sum();
+		
+		return Utils.round(total);
+	}
 }
